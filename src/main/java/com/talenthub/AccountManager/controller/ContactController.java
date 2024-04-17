7c40e3ab03d095dc7142581c19aff98fb7f7cefa
Wilermo <william.dmo.sec@gmail.com>
2024-04-17 16:23:47 -0500

    rm Cors

--- src/main/java/com/talenthub/AccountManager/controller/ContactController.java
+++ src/main/java/com/talenthub/AccountManager/controller/ContactController.java
@@ -16,6 +16,7 @@ public class ContactController {
     ContactService contactService;
 
     //@CrossOrigin(origins = {"https://talenthubsoftware.vercel.app","http://localhost:8080", "http://localhost:4200", "https://canelaapigatewayback-production.up.railway.app"})
+    @CrossOrigin(origins = "*")
     @PostMapping("/send")
     public ContactInfo sendMessage(@RequestBody ContactInfo contactInfo) {
         System.out.println(contactInfo.toString());
