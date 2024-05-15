fc4fb38b7407805c6d02bf2f582155436eb29d6c
ticaso <ticaso999@gmail.com>
2024-05-14 19:30:57 -0500

    Orjuela es mk

--- src/main/java/com/talenthub/AccountManager/controller/CompanyController.java
+++ src/main/java/com/talenthub/AccountManager/controller/CompanyController.java
@@ -1,54 +1,80 @@
 package com.talenthub.AccountManager.controller;
 
 import com.talenthub.AccountManager.model.Company;
+import com.talenthub.AccountManager.payload.response.CompanyResponse;
 import com.talenthub.AccountManager.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
+@Validated
 @RestController
-@RequestMapping("/companies")
+@RequestMapping("/api/talentsoft/company")
 public class CompanyController {
 
+    private final CompanyService companyService;
+
     @Autowired
-    private CompanyService companyService;
-
-    @GetMapping
-    public ResponseEntity<List<Company>> getAllCompanies() {
-        return ResponseEntity.ok(companyService.findAll());
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Company> getCompanyById(@PathVariable int id) {
-        Optional<Company> company = companyService.findById(id);
-        return company.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    @CrossOrigin
+    @PostMapping("/save")
+    public Company sendMessage(@RequestBody Company company) {
+        System.out.println(company.toString());
+        return companyService.save(company);
     }
 
-    @PostMapping
-    public ResponseEntity<Company> createCompany(@RequestBody Company company) {
-        return ResponseEntity.ok(companyService.save(company));
+    @CrossOrigin
+    @GetMapping("/list")
+    public List<Company> findAll() {
+        return companyService.findAll();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Company> updateCompany(@PathVariable int id, @RequestBody Company company) {
-        if (companyService.findById(id).isPresent()) {
-            company.setId(id);
-            return ResponseEntity.ok(companyService.update(company));
-        } else {
-            return ResponseEntity.notFound().build();
+    @GetMapping("/{email}")
+    public ResponseEntity<CompanyResponse> getCompany(@PathVariable String email) {
+        try {
+            return ResponseEntity.ok(companyService.getCompanyId(email));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteCompany(@PathVariable int id) {
-        if (companyService.findById(id).isPresent()) {
-            companyService.deleteById(id);
-            return ResponseEntity.ok().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+
+
+
+
+
+
+//
+//    @PostMapping
+//    public String save(@Valid @RequestBody CompanyVO vO) {
+//        return companyService.save(vO).toString();
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public void delete(@Valid @NotNull @PathVariable("id") Integer id) {
+//        companyService.delete(id);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public void update(@Valid @NotNull @PathVariable("id") Integer id,
+//                       @Valid @RequestBody CompanyUpdateVO vO) {
+//        companyService.update(id, vO);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public CompanyDTO getById(@Valid @NotNull @PathVariable("id") Integer id) {
+//        return companyService.getById(id);
+//    }
+//
+//    @GetMapping
+//    public Page<CompanyDTO> query(@Valid CompanyQueryVO vO) {
+//        return companyService.query(vO);
+//    }
 }
