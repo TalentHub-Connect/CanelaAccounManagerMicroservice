2b710067ce8f327153355d90d145862cc70012f8
ticaso <ticaso999@gmail.com>
2024-05-25 21:48:06 -0500

    changes for plan controller

--- src/main/java/com/talenthub/AccountManager/controller/PlanController.java
+++ src/main/java/com/talenthub/AccountManager/controller/PlanController.java
@@ -23,10 +23,10 @@ public class PlanController {
         return planService.findAllPlans();
     }
 
-    @GetMapping("/list")
+   /*  @GetMapping("/list")
     public List<Plan> findAll() {
         return planService.findAllPlans();
-    }
+    }*/
 
 
     @GetMapping("/{id}")
